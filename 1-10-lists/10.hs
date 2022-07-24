4fe51d359af7f6ba48332a5100ae57205c5eff6f
B4mbus <peaceofloneliness@gmail.com>
2022-07-24 13:10:56 +0200

    feat: 1-10:10 solved.

---
+++ 1-10-lists/10.hs
@@ -0,0 +1,22 @@
+{-
+(*) Run-length encoding of a list.
+Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+
+Example in Haskell:
+
+λ> encode "aaaabccaadeeee"
+[(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+-}
+
+pack :: Eq a => [a] -> [[a]]
+pack (x:xs) = (x:first) : pack rest where
+  (first, rest) = span (==x) xs
+pack [] = []
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = f <$> pack xs where
+  f y@ys = (length ys, y)
+
+main :: IO ()
+main = print $ encode "aaaaaabbbbccc"
