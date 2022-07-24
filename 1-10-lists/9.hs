aa4af461fa42f704b8b5a6e7ce66172976e505c2
B4mbus <peaceofloneliness@gmail.com>
2022-07-24 12:57:38 +0200

    feat: 1-10:9 solved.

---
+++ 1-10-lists/9.hs
@@ -0,0 +1,16 @@
+import Data.List
+{-
+(**) Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+
+Example in Haskell:
+λ> pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
+["aaaa","b","cc","aa","d","eeee"]
+-}
+
+pack :: Eq a => [a] -> [[a]]
+pack (x:xs) = (x:first) : pack rest where
+  (first, rest) = span (==x) xs
+pack [] = []
+
+main :: IO ()
+main = print $ pack "aaaaaabbbbccc"
